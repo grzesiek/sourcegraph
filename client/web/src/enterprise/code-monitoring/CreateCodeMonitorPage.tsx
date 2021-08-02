14ebbd4b184397c951cd5e5ee56e10140eb2bfcc
Farhan Attamimi <farhan@sourcegraph.com>
2021-08-02 13:05:19 +0800

    eventLogger: Create Code Monitor Submit event

--- client/web/src/enterprise/code-monitoring/CreateCodeMonitorPage.tsx
+++ client/web/src/enterprise/code-monitoring/CreateCodeMonitorPage.tsx
@@ -36,8 +36,9 @@ const AuthenticatedCreateCodeMonitorPage: React.FunctionComponent<CreateCodeMoni
     ])
 
     const createMonitorRequest = useCallback(
-        (codeMonitor: CodeMonitorFields): Observable<Partial<CodeMonitorFields>> =>
-            createCodeMonitor({
+        (codeMonitor: CodeMonitorFields): Observable<Partial<CodeMonitorFields>> => {
+            eventLogger.log('CreateCodeMonitorFormSubmitted')
+            return createCodeMonitor({
                 monitor: {
                     namespace: authenticatedUser.id,
                     description: codeMonitor.description,
@@ -53,7 +54,8 @@ const AuthenticatedCreateCodeMonitorPage: React.FunctionComponent<CreateCodeMoni
                         header: '',
                     },
                 })),
-            }),
+            })
+        },
         [authenticatedUser.id, createCodeMonitor]
     )
 
